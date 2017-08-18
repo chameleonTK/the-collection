5ed19e0cf9b6d57e8ba15b0323bb1d44666caa9a
Pakawat Nakwijit <kramatk@gmail.com>
2017-08-18 14:20:47 +0700

    [env] set angular4

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,62 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
-var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
+const elixir = require('laravel-elixir');
 
-var rev = require("gulp-rev");
+require('laravel-elixir-vue');
+require('laravel-elixir-ts');
 
-gulp.task('scss', function() {
-    return gulp.src([
-          'resources/assets/scss/*.scss',
-        ])
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(concat('app.css'))
-        .pipe(minifyCss())
-        .pipe(rename('app.min.css'))
-        .pipe(gulp.dest('public/css/'));
-});
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
 
-// Concatenate & Minify JS
-gulp.task('js', function() {
-    return gulp.src([
-            'resources/assets/js/app.js',
-            'resources/assets/js/**/*.js'
-        ])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('public/js/'))
-        .pipe(uglify())
-        .on('error', gutil.log)
-        .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('public/js/'));
-});
+elixir(function(mix) {
+    mix.sass('app.scss')
+        .webpack('app.js')
+        .copy('node_modules/@angular', 'public/js/@angular')
+        .copy('node_modules/anular2-in-memory-web-api', 'public/js/anular2-in-memory-web-api')
+        .copy('node_modules/core-js', 'public/js/core-js')
+        .copy('node_modules/reflect-metadata', 'public/js/reflect-metadata')
+        .copy('node_modules/systemjs', 'public/js/systemjs')
+        .copy('node_modules/rxjs', 'public/js/rxjs')
+        .copy('node_modules/zone.js', 'public/js/zone.js')
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('resources/assets/js/**/*.js', ['js']);
-    gulp.watch('resources/assets/scss/*.scss', ['scss']);
-});
-
-// Default Task
-gulp.task('default', ['scss','js', 'watch']);
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    .typescript("resources/assets/typescript/", "public/app", {
+        "target": "es5",
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "sourceMap": true,
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "lib": ["es2017", "dom"],
+        "removeComments": false,
+        "noImplicitAny": false,
     });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+
+
+// const gulp = require('gulp');
+// // const del = require('del');
+// const typescript = require('gulp-typescript');
+// const tscConfig = require('./tsconfig.json');
+
+// clean the contents of the distribution directory
+// gulp.task('clean', function () {
+//   return del('dist/**/*');
+// });
+
+// TypeScript compile
+/*gulp.task('compile', function () {
+  return gulp
+    .src('resources/assets/typescript/*.ts')
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(gulp.dest('public/app'));
 });
 
-gulp.task("revision", function(){
-  return gulp.src([
-      "public/css/app.min.css",
-      "public/js/app.min.js",
-      "public/js/app-template.js",
-    ],{base: 'public'})
-    .pipe(rev())
-    .pipe(gulp.dest("public"))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest("public/build/"))
-})
+gulp.task('build', ['compile']);
+gulp.task('default', ['build']);*/
